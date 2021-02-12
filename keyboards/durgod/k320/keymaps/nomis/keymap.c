3a78b86f4b3a01dda6744b91a217f93787cf2604
Simon Arlott <sa.me.uk>
2021-02-12 20:44:25 +0000

    Add numlock

--- keyboards/durgod/k320/keymaps/nomis/keymap.c
+++ keyboards/durgod/k320/keymaps/nomis/keymap.c
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,            KC_DEL,   KC_END,   KC_PGDN,
       KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NUHS,  KC_ENT,
       KC_LSFT,  KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,            KC_UP,
-      KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RGUI, MO(_FUNC),KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT
+      KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RGUI, MO(_FUNC), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT
   ),
   /* Keymap _FUNC: Function Layer
    * ,-----------------------------------------------------------.  ,--------------.
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------------------------------------------------------.    |  `--------------'
    * |      |   |Slp|   |   |   |   |   |   |   |   |   |   |    |
    * |-----------------------------------------------------------|       ,----.
-   * |    |   |   |   |Cal|   |   |   |   |   |   |   |          |       |Vol+|
+   * |    |   |   |   |Cal|   |   |NlK|   |   |   |   |          |       |Vol+|
    * |-----------------------------------------------------------|  ,--------------.
    * |    |Lock|    |                       |    |App  |Func|    |  |Prev|Vol-|Next|
    * `-----------------------------------------------------------'  `--------------'
@@ -71,8 +71,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       RESET,    KC_MPLY,  KC_MSTP,  KC_MRWD,  KC_MFFD,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,  _______,  _______,  _______,  _______,            _______,  _______,  KC_PAUS,
       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_MSEL,  _______,
       _______,  _______,  _______,  KC_MAIL,  _______,  _______,  _______,  _______,  _______,  _______,  KC_PWR,   _______,  _______,            _______,  KC_MUTE,  _______,
-      _______,  _______,  KC_SLEP,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-      _______,  _______,  _______,  _______,  KC_CALC,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,            KC_VOLU,
+      _______,  _______,  KC_SLEP,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  TG(_NUM), _______,
+      _______,  _______,  _______,  _______,  KC_CALC,  _______,  _______,  KC_NLCK,  _______,  _______,  _______,  _______,            _______,            KC_VOLU,
       _______,  KC_TGUI,  _______,                                _______,                                _______,  KC_APP,   _______,  _______,  KC_MRWD,  KC_VOLD,  KC_MFFD
   ),
 };
