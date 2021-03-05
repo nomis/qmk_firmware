a169bfdfed59f2b4992a5a613b97091e2eb29be0
Simon Arlott <sa.me.uk>
2021-03-05 13:17:41 +0000

    np21 reconfigure

--- keyboards/ymdk_np21/config.h
+++ keyboards/ymdk_np21/config.h
@@ -79,6 +79,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
 
+#define USB_POLLING_INTERVAL_MS 10
+
 /* define if matrix has ghost (lacks anti-ghosting diodes) */
 //#define MATRIX_HAS_GHOST
 
