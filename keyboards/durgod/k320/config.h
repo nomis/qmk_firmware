3b4e4bbab57044b56b7c7882e6201c449cd81a6d
Simon Arlott <sa.me.uk>
2021-03-05 13:17:23 +0000

    durgod reconfigure

--- keyboards/durgod/k320/config.h
+++ keyboards/durgod/k320/config.h
@@ -37,7 +37,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DIODE_DIRECTION ROW2COL
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
-#define DEBOUNCE 7
+#define DEBOUNCE 5
+#define USB_POLLING_INTERVAL_MS 1
 
 /* Bootmagic Lite key configuration */
 #define EARLY_INIT_PERFORM_BOOTLOADER_JUMP TRUE
---
+++ keyboards/durgod/k320/keymaps/nomis/rules.mk
@@ -0,0 +1,2 @@
+NKRO_ENABLE = no
+DEBOUNCE_TYPE = sym_defer_pk
