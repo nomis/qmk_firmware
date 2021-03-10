0845f378dd69e7b62e9b74b3518a48632f80261a
Simon Arlott <sa.me.uk>
2021-03-10 11:19:14 +0000

    Change KC_WSCH to KC_WHOM

--- keyboards/durgod/k320/keymaps/nomis/keymap.c
+++ keyboards/durgod/k320/keymaps/nomis/keymap.c
@@ -28,7 +28,7 @@ static bool win_key_locked = false;
 enum custom_keycodes {
 	CK_TGUI = SAFE_RANGE,   // Toggle between GUI Lock or Unlock
 	CK_PAUS,                // KC_MPLY (or KC_PAUS when modifiers held)
-	CK_SRCH,                // KC_WSCH (or KC_SLCK when modifiers held)
+	CK_SLCK,                // KC_WHOM (or KC_SLCK when modifiers held)
 	CK_RATE,
 	CK_STRF,
 	CK_STRL,
@@ -43,7 +43,7 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	/* Keymap L_BASE: Base Layer (Default Layer)
 	 * ,-----------------------------------------------------------.  ,--------------.
-	 * |Esc  |f1| f2| f3| f4|  | f5| f6| f7| f8|   | f9|f10|f11|f12|  |Prnt|Srch|Play|
+	 * |Esc  |f1| f2| f3| f4|  | f5| f6| f7| f8|   | f9|f10|f11|f12|  |Prnt| WWW|Play|
 	 * |-----------------------------------------------------------|  |--------------|
 	 * | ` | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |  0| - | = |Backsp |  | Ins|Home|PgUp|
 	 * |-----------------------------------------------------------|  |--------------|
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	 * `-----------------------------------------------------------'  `--------------'
 	 */
 	[L_BASE] = LAYOUT_tkl_iso(
-		KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,             KC_PSCR,  CK_SRCH,  CK_PAUS,
+		KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,             KC_PSCR,  CK_SLCK,  CK_PAUS,
 		KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,
 		KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,            KC_DEL,   KC_END,   KC_PGDN,
 		KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NUHS,  KC_ENT,
@@ -145,12 +145,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 			process_action(record, action);
 			return false;
 
-		case CK_SRCH:
+		case CK_SLCK:
 			if (record->event.pressed) {
 				if (get_mods() & MOD_MASK_CSAG) {
 					srch_keycode = KC_SLCK;
 				} else {
-					srch_keycode = KC_WSCH;
+					srch_keycode = KC_WHOM;
 				}
 			}
 
