df4301b1c8814f4269cbfb6894abf4bf69d127b1
Simon Arlott <sa.me.uk>
2021-03-11 22:42:37 +0000

    Move USB polling config

--- keyboards/durgod/k320/config.h
+++ keyboards/durgod/k320/config.h
@@ -38,7 +38,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
-#define USB_POLLING_INTERVAL_MS 1
 
 /* Bootmagic Lite key configuration */
 #define EARLY_INIT_PERFORM_BOOTLOADER_JUMP TRUE
