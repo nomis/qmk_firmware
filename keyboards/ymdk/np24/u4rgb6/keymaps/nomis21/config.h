654c630c73d3841511ad784f7c28fd4a9bc69bde
Simon Arlott <sa.me.uk>
2021-03-14 11:37:19 +0000

    keymaps: Add NP21 layout

---
+++ keyboards/ymdk/np24/u4rgb6/keymaps/nomis21/config.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#undef VENDOR_ID
+#undef PRODUCT_ID
+
+#define VENDOR_ID      0x1209
+#define PRODUCT_ID     0x0002
+
+#undef PRODUCT
+#define PRODUCT         NP21 U4 RGB6
+
+#undef RGBLIGHT_ANIMATIONS
+#undef RGBLIGHT_DEFAULT_MODE
+#define RGBLIGHT_LAYERS
+#define RGBLIGHT_LAYER_BLINK
+#define RGBLIGHT_LAYERS_OVERRIDE_RGB_OFF
