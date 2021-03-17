f907ff7b74b266e1c4b6d29bab373e93db0d3a66
Simon Arlott <sa.me.uk>
2021-03-17 14:51:52 +0000

    asym debounce_free

--- quantum/debounce/asym_eager_defer_pk.c
+++ quantum/debounce/asym_eager_defer_pk.c
@@ -63,9 +63,6 @@ static void transfer_matrix_values(matrix_row_t raw[], matrix_row_t cooked[], ui
 
 // we use num_rows rather than MATRIX_ROWS to support split keyboards
 void debounce_init(uint8_t num_rows) {
-#ifdef UNIT_TEST
-    free(debounce_counters);
-#endif
     debounce_counters = malloc(num_rows * MATRIX_COLS * sizeof(debounce_counter_t));
     int i = 0;
     for (uint8_t r = 0; r < num_rows; r++) {
@@ -73,6 +70,10 @@ void debounce_init(uint8_t num_rows) {
             debounce_counters[i++].time = DEBOUNCE_ELAPSED;
         }
     }
+}
+
+void debounce_free(void) {
+    free(debounce_counters);
 }
 
 void debounce(matrix_row_t raw[], matrix_row_t cooked[], uint8_t num_rows, bool changed) {
